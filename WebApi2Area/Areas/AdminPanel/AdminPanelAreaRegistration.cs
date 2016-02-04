90d5c5e32913335534b961a15c4e5f377bc4e166
Shashangka Shekhar <shashangka@gmail.com>
2016-02-04 09:20:37 +0600

    Upload and share

---
+++ WebApi2Area/Areas/AdminPanel/AdminPanelAreaRegistration.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApi2Area.Areas.AdminPanel
+{
+    public class AdminPanelAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "AdminPanel";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            //****************=======Default Api Route=========*******************
+
+            context.Routes.MapHttpRoute(
+                name: "AdminPanelApiAction",
+                routeTemplate: "AdminPanel/api/{controller}/{action}",
+                defaults: new { id = UrlParameter.Optional }
+            );
+
+            context.Routes.MapHttpRoute(
+                name: "AdminPanelApi",
+                routeTemplate: "AdminPanel/api/{controller}"
+            );
+
+            //****************=======Default Route=========*******************
+            context.MapRoute(
+                "AdminPanel_dashboard",
+                "AdminPanel/{controller}/{action}/{id}",
+                new { Controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
+            );
+
+            context.MapRoute(
+                "AdminPanel_default",
+                "AdminPanel/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApi2Area/Areas/AdminPanel/apiControllers/DashboardController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2Area.Areas.AdminPanel.apiControllers
+{
+    public class DashboardController : ApiController
+    {
+        //GET: AdminPanel/api/Dashboard/
+        [HttpGet]
+        public string Get()
+        {
+            string result = string.Empty;
+            try
+            {
+                result = "We are in Dashboard From API2";
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+            return result;
+        }
+
+        //GET: AdminPanel/api/Dashboard/Get/1
+        [Route("AdminPanel/api/Dashboard/Get/{id:int}")]
+        [HttpGet]
+        public string Get(int? id)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = "We are in Member - " + id + " From API2";
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+            return result;
+        }
+    }
+}
